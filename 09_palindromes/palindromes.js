8bc859708dc42d8cb4e18943ceb70ed8c21efaa2
Samiran Dutta <samiran.dutta71@gmail.com>
2025-05-04 14:11:25 +0530

    Palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let reverseStr = str.toLowerCase().split('').filter( ele => ele >='a' && ele <= 'z' || ele >='1' && ele <= '9').reverse().join('');
+    let newStr = str.toLowerCase().split('').filter( ele => ele >='a' && ele <= 'z' || ele >='1' && ele <= '9').join('');
+    return reverseStr === newStr;
 };
-
+console.log(palindromes('Race car!'));
 // Do not edit below this line
 module.exports = palindromes;
